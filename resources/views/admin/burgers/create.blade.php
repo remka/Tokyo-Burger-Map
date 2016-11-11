29267aeab24175e4dd2dd2c74422791ecb69af11
remka <remka@users.noreply.github.com>
2016-11-11 19:20:29 +0900

    A bit more CRUD for burgers...

--- resources/views/admin/burgers/create.blade.php
+++ resources/views/admin/burgers/create.blade.php
@@ -31,6 +31,76 @@
 
       <h1 class="page-header">Create a burger</h1>
 
+      @if (count($errors) > 0)
+        <div class="alert alert-danger">
+    			<strong>Whoops!</strong> There were some problems with your input.<br><br>
+    			<ul>
+    				@foreach ($errors->all() as $error)
+    					<li>{{ $error }}</li>
+    				@endforeach
+    			</ul>
+    		</div>
+    	@endif
+
+      {!! Form::open(array('route' => 'admin.burgers.store','method'=>'POST','class' => 'form-horizontal')) !!}
+
+      <div class="form-group">
+        <label for="title_en" class="col-sm-2 control-label">Name (EN)</label>
+        <div class="col-sm-10">
+          {!! Form::text('title_en', null, array('placeholder' => 'Ex: THE GREAT BURGER','class' => 'form-control','id' => 'title_en')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="title_ja" class="col-sm-2 control-label">Name (JA)</label>
+        <div class="col-sm-10">
+          {!! Form::text('title_ja', null, array('placeholder' => 'Ex: ザ グレートバーガー','class' => 'form-control','id' => 'title_ja')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="description" class="col-sm-2 control-label">Description</label>
+        <div class="col-sm-10">
+          {!! Form::textarea('description', null, array('placeholder' => 'A short description','class' => 'form-control','id' => 'description')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="latitude" class="col-sm-2 control-label">Latitude</label>
+        <div class="col-sm-10">
+          {!! Form::text('latitude', null, array('placeholder' => '35.666216','class' => 'form-control','id' => 'latitude')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="longitude" class="col-sm-2 control-label">Longitude</label>
+        <div class="col-sm-10">
+          {!! Form::text('longitude', null, array('placeholder' => '139.704692','class' => 'form-control','id' => 'longitude')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="address_1" class="col-sm-2 control-label">Address 1</label>
+        <div class="col-sm-10">
+          {!! Form::text('address_1', null, array('placeholder' => '6-12-5 Jingumae','class' => 'form-control','id' => 'address_1')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="address_2" class="col-sm-2 control-label">Address 2</label>
+        <div class="col-sm-10">
+          {!! Form::text('address_2', null, array('placeholder' => 'Sunny Plaza Bldg.','class' => 'form-control','id' => 'address_2')) !!}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-8 col-sm-offset-2">
+          <button type="submit" class="btn btn-primary btn-lg">Create new burger</button>
+        </div>
+      </div>
+
+      {!! Form::close() !!}
+
     </div>
   </div>
 </div>
